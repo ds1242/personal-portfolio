88133c6565994cb72155969efb8d7b83cc007860
dshaw1242 <david.shaw1242@gmail.com>
2021-06-23 15:01:55 -0600

    setup server to start and listen

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const hbs = exphbs.create();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'super secret string',
+    cookie: {},
+    resave: false,
+    saveUnitialized: false,
+    maxAge: 1 * 30 * 60 * 1000,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
+});
